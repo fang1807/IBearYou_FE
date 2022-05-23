fd9de2401eff102043f8b0739772eaca6fce6423
atty <Aratitraya.atty@mail.kmutt.ac.th>
2022-05-24 00:05:35 +0700

    [delete library]

--- CalendarScreen.js
+++ CalendarScreen.js
@@ -13,13 +13,7 @@ import {setCurrentDiaryID,setCurrentDate} from './actions/Diary'
 
 
 import {Calendar, LocaleConfig} from 'react-native-calendars';
-  /*LocaleConfig.locales['th'] = {
-    monthNames: ['มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม'],
-    monthNamesShort: ['ม.ค.','ก.พ.','มี.ค.','เม.ย.','พ.ค.','มิ.ย.','ก.ค.','ส.ค.','ก.ย.','ต.ค.','พ.ย.','ธ.ค.'],
-    dayNames: ['อาทิตย์','จันทร์','อังคาร','พุธ','พฤหัสบดี','ศุกร์','เสาร์'],
-    dayNamesShort: ['อา.','จ.','อ.','พ.','พฤ.','ศ.','ส.']
-  }
-  LocaleConfig.defaultLocale = 'th';*/
+
 
 class CalendarScreen extends Component {
   constructor(props) {
